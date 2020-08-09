773a26332166b6f8c2683c444cc9062ab943a8b7
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2020-08-09 04:22:06 +0200

    Fetch information from express API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { FiExternalLink } from 'react-icons/fi'
@@ -47,69 +47,75 @@ const ExternalLink = ({ pathname, text }) => (
 )
 
 const Home = () => {
-  const thisYear = new Date().getFullYear()
+  const [index, setIndex] = useState('')
+  useEffect(() => {
+    const getIndex = async () => {
+      try {
+        const fetchedIndex = await fetch(
+          'http://localhost:3333',
+        ).then((result) => result.json())
+        setIndex(fetchedIndex)
+      } catch {
+        console.log('cannot fetch')
+      }
+    }
+    getIndex()
+  }, [])
+
+  if (!index) return <Centered>loading...</Centered>
+
   return (
     <Centered>
-      <H1>This is JS-ramverk</H1>
+      <H1>{index.main_heading}</H1>
       <P>
-        I&apos;m Richard and this is the first page for the course{' '}
-        <ExternalLink pathname='https://www.jsramverk.se' text='jsramverk' />.
-      </P>
-      <Image src='https://avatars1.githubusercontent.com/u/31686265?s=460&u=b237572a2ca661ec50a7e2a4ce362d3ce67e751f&v=4' />
-      <P style={{ alignSelf: 'flex-start' }}>
-        I am currently {thisYear - 1986} years old and residing in Helsingborg.
-      </P>
-      <P>
-        I have been studying web development remotely for a few years now. I
-        actually did my first attempt in 2014, trying to complete the
-        introductory Python course at{' '}
+        {index.paragraph0.text}{' '}
         <ExternalLink
-          pathname='https://www.bth.se'
-          text='Blekinge Tekniska Högskola'
-        />{' '}
-        (BTH). That time I did not finish the course but in 2018 I came back in
-        almost full force and gave studying another shot.
-      </P>
-      <P>
-        Since 2018 I have studied the &quot;course package&quot;{' '}
-        <ExternalLink
-          pathname='https://www.bth.se/kurspaket/KP667/20202/'
-          text='Webbprogrammering och Databaser'
-        />{' '}
-        and in 2019 I started studying the full{' '}
-        <ExternalLink
-          text='program'
-          pathname='https://www.bth.se/utbildning/program/pagwg/'
+          pathname={index.paragraph0.link.url}
+          text={index.paragraph0.link.text}
         />
-        . Right now I have completed enough points for almost one year (I have a
-        few points here and there to collect from the first year).
+        .
       </P>
+      <Image src={index.github_avatar.url} />
+      <P style={{ alignSelf: 'flex-start' }}>{index.paragraph1.text}</P>
       <P>
-        While studying at BTH I have also been studying other courses at other
-        swedish universities such as Umeå Universitet and Uppsala Universitet.
-      </P>
-      <P>
-        During the last summer I have been hard at work studying{' '}
+        {index.paragraph2.first_part.text}
         <ExternalLink
-          pathname='https://www.fullstackopen.com/en'
-          text='Fullstack Open'
+          pathname={index.paragraph2.first_part.link.url}
+          text={index.paragraph2.first_part.link.text}
         />{' '}
-        and at the moment I have completed the first 7 parts of the program and
-        I am looking forward to continue with the GraphQL and TypeScript parts
-        as time permits during the fall.
+        {index.paragraph2.second_part.text}
       </P>
       <P>
-        I also recently found a very interesting book at O&apos;Reilly which is
-        called{' '}
+        {index.paragraph3.first_part.text}
         <ExternalLink
-          pathname='https://www.oreilly.com/library/view/javascript-everywhere/9781492046974/'
-          text='JavaScript Everywhere'
-        />{' '}
-        that I also plan to go through as time permits during the fall.
+          pathname={index.paragraph3.first_part.link.url}
+          text={index.paragraph3.first_part.link.text}
+        />
+        {index.paragraph3.second_part.text}
+        <ExternalLink
+          text={index.paragraph3.second_part.link.text}
+          pathname={index.paragraph3.second_part.link.url}
+        />
+        {index.paragraph3.third_part.text}
       </P>
-      <P style={{ alignSelf: 'flex-start' }}>
-        I am looking forward to diving into more JavaScript frameworks.
+      <P>{index.paragraph4.text}</P>
+      <P>
+        {index.paragraph5.first_part.text}
+        <ExternalLink
+          pathname={index.paragraph5.first_part.link.url}
+          text={index.paragraph5.first_part.link.text}
+        />
+        {index.paragraph5.second_part.text}
       </P>
+      <P>
+        {index.paragraph6.first_part.text}
+        <ExternalLink
+          pathname={index.paragraph6.first_part.link.url}
+          text={index.paragraph6.first_part.link.text}
+        />
+        {index.paragraph6.second_part.text}
+      </P>
+      <P style={{ alignSelf: 'flex-start' }}>{index.paragraph7.text}</P>
     </Centered>
   )
 }
